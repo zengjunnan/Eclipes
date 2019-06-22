3a97b240423ef2135f70d04f2a777203232eac2d
601063512@qq.com <601063512@qq.com>
2019-06-22 16:28:21 +0800

    ok

---
+++ src/cn/itcast/Eclispe/Demo1.java
@@ -0,0 +1,16 @@
+package cn.itcast.Eclispe;
+
+import java.util.List;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("11111");
+			
+			List<Object>list=null;
+			
+			
+
+	}
+}
